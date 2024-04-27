eedceded5b5a1bddf0fae3847b72d5ba4f73f20a
AieryAir <104162923+AieryAir@users.noreply.github.com>
2024-04-27 11:38:18 +0800

    add calculator operations
    Case switch for all four basic operations.

--- main.cpp
+++ main.cpp
@@ -24,5 +24,54 @@ int main () {
     cout << "Please input the second number: " << endl;
     cin >> userInput2;
 
+    double result;
+
+    switch (userChoice) {
+
+        case 1: 
+
+        result = userInput1 + userInput2;
+
+        cout << "The sum of " << userInput1 << " and " << userInput2 << " is... " << endl;
+        cout << result;
+
+        break;
+
+
+        case 2:
+
+        result = userInput1 - userInput2;
+
+        cout << "The difference between " << userInput1 << " and " << userInput2 << " is... " << endl;
+        cout << result;
+
+        break; 
+
+        
+        case 3: 
+
+        result = userInput1 * userInput2;
+
+        cout << "The product of " << userInput1 << " and " << userInput2 << " is... " << endl;
+        cout << result;
+
+        break;
+
+
+        case 4: 
+
+        result = userInput1 / userInput2;
+
+        cout << "The quotient of " << userInput1 << " and " << userInput2 << " is... " << endl;
+        cout << result;
+
+        break;
+        
+        default: 
+
+        cout << "Error. Please try again." << endl;
+
+    }
+
     return 0;
 }
